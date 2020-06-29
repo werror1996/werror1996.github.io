b702dccb3cbbceef3c26e904828d8c9b290a4cfe
werror1996 <werror634@gmail.com>
2020-06-30 01:32:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,16 +28,15 @@ function showSlides(n) {
 }
 
 
-const navSlide = () => {
-    const burger = document.querySelector('.mobile_menu');
-    const nav = document.querySelector('.menu');
+const burger = document.querySelector('.mobile_menu');
+const nav = document.querySelector('.menu');
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('menu_active')
-    })
-}
+burger.addEventListener('click', () => {
+    nav.classList.toggle('menu_active')
+})
 
-navSlide();
+
+
 
 
 
